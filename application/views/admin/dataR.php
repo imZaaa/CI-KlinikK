5a0f9d2f2e7c7e9712b9bd45a936fecf74fc725f
Rheza <rhezarifalsya266@gmail.com>
2025-01-16 00:33:22 +0700

    p

--- application/views/admin/dataR.php
+++ application/views/admin/dataR.php
@@ -137,7 +137,7 @@
                            <i class="fs-4 bi-clipboard-data-fill"></i><span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
                     </li>
                     <li>
-                        <a href="#" class="nav-link px-0 align-middle active">
+                        <a href="#" class="nav-link px-0 align-middle ">
                            <i class="fs-4 bi-person-fill-add"></i> <span class="ms-1 d-none d-sm-inline">Dokter</span> </a>
                     </li>
                     <li>
@@ -149,7 +149,7 @@
                              <i class="fs-4 bi-person-bounding-box"></i> <span class="ms-1 d-none d-sm-inline">Profile</span></a>
                     </li>
                     <li>
-                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle active">
                             <i class="fs-4 bi-folder-fill"></i> <span class="ms-1 d-none d-sm-inline">Data</span></a>
                         <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
                             <li class="w-100">
